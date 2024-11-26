12cb0c5ba64145f1f3721e4a161d99d104dedfc9
tuliomitico <tulioneedforspeed@gmail.com>
2024-11-25 21:02:06 -0300

    chore: add react configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,15 @@
 import love from "eslint-config-love";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   love,
   {
+    settings: {
+      react: { version: "detect" },
+    },
     languageOptions: {
       parserOptions: {
         project: "./tsconfig-eslint.json",
@@ -17,8 +21,11 @@ export default [
       "@typescript-eslint/class-methods-use-this": "off",
       "@typescript-eslint/unbound-method": "off",
       "@typescript-eslint/no-magic-numbers": "off",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
     },
     ignores: ["node_modules", "dist", "coverage", "build", ".vscode"],
   },
+  reactPlugin.configs.recommended,
   eslintPluginPrettier,
 ];
