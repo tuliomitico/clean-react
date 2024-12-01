9bd4243a0442806bb43a312fde9a9825e38f3fae
tuliomitico <tulioneedforspeed@gmail.com>
2024-12-01 15:13:39 -0300

    test: ensure Login will not render spinner and error on start

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,27 @@
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.{ts,tsx}',
-      '!<rootDir>/src/main/**/*',
-      '!<rootDir>/src/**/index.ts',
-      '!**/*.d.ts'
-    ],
-    coverageDirectory: 'coverage',
-    testPathIgnorePatterns: [
-      '<rootDir>/node_modules/',
-      '<rootDir>/tests/e2e/cypress'
-    ],
-    testEnvironment: 'jsdom',
-    transform: {
-      '.+\\.(ts|tsx)$': 'ts-jest'
-    },
-    moduleNameMapper: {
-      '@/tests/(.*)': '<rootDir>/tests/$1',
-      '@/(.*)': '<rootDir>/src/$1',
-      '\\.scss$': 'identity-obj-proxy'
-    }
-  }
+  roots: ["<rootDir>/src"],
+  setupFiles: ["<rootDir>/jest.setup.js"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{ts,tsx}",
+    "!<rootDir>/src/main/**/*",
+    "!<rootDir>/src/**/index.ts",
+    "!**/*.d.ts",
+  ],
+  coverageDirectory: "coverage",
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/tests/e2e/cypress",
+  ],
+  testEnvironment: "jsdom",
+  transform: {
+    ".+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/tests/(.*)": "<rootDir>/tests/$1",
+    "@/(.*)": "<rootDir>/src/$1",
+    "\\.scss$": "identity-obj-proxy",
+  },
+};
