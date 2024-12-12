84fce592ed03ca814d0a9638b6acfa9de8157fec
tuliomitico <tulioneedforspeed@gmail.com>
2024-12-11 21:40:14 -0300

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -80,4 +80,18 @@ describe("Login Component", () => {
     expect(passwordStatus.title).toBe("Tudo certo!");
     expect(passwordStatus.textContent).toBe("🟢");
   });
+  test("Should enable submit button if form is valid", () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = "";
+    const emailInput = sut.getByTestId("email");
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.email() },
+    });
+    const passwordInput = sut.getByTestId("password");
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() },
+    });
+    const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(false);
+  });
 });
