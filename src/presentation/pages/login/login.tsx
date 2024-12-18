9e477aca8529f4e6ce347f59327be26ce4663cdb
tuliomitico <tulioneedforspeed@gmail.com>
2024-12-17 21:09:29 -0300

    feat: ensure Login add accessToken to localstorage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -43,10 +43,11 @@ export function Login({
         return;
       }
       setState({ ...state, isLoading: true });
-      await authentication?.auth({
+      const account = await authentication?.auth({
         email: state.email,
         password: state.password,
       });
+      localStorage.setItem("accessToken", account?.accessToken ?? "");
     } catch (error) {
       setState({
         ...state,
