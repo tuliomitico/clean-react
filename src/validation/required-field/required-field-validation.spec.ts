0fce463eafcb8b43c8823a536188422765c90f48
tuliomitico <tulioneedforspeed@gmail.com>
2024-12-29 16:28:55 -0300

    test: ensure RequiredFieldValidation returns false if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,16 @@
 import { RequiredFieldValidation } from "@/validation/required-field/requires-field-validation";
 import { RequiredFieldError } from "@/validation/errors";
+import { faker } from "@faker-js/faker";
 
 describe("RequiredFieldValidation", () => {
   test("Should return error if field is empty", () => {
     const sut = new RequiredFieldValidation("email");
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
+  });
+  test("Should return falsy if field is not empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate(faker.lorem.word());
+    expect(error).toBeFalsy();
   });
 });
