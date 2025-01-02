3ae733d93fa16caebd630b78a2c734b792581657
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-02 20:46:58 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,16 @@
 import { InvalidFieldError } from "@/validation/errors";
 import { EmailValidation } from "@/validation/validators/email/email-validation";
+import { faker } from "@faker-js/faker";
 
 describe("EmailValidation", () => {
   test("Should return error if email is invalid", () => {
-    const sut = new EmailValidation("email");
-    const error = sut.validate("");
+    const sut = new EmailValidation(faker.lorem.word());
+    const error = sut.validate(faker.lorem.word());
     expect(error).toEqual(new InvalidFieldError());
+  });
+  test("Should return falsy if email is valid", () => {
+    const sut = new EmailValidation(faker.lorem.word());
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
