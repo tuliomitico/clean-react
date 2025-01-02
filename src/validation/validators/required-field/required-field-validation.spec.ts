7196e5f83e04cfcaf0f62782c922f8e731d1f96a
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-02 20:18:23 -0300

    refactor: move required field validation to a different folder

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from "@/validation/required-field/requires-field-validation";
+import { RequiredFieldValidation } from "@/validation/validators/required-field/requires-field-validation";
 import { RequiredFieldError } from "@/validation/errors";
 import { faker } from "@faker-js/faker";
 
