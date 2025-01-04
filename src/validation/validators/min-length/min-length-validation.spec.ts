7542ef77f936ee7eaf8ea535dce84f6d20a43e72
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-04 11:02:26 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,9 +1,9 @@
 import { InvalidFieldError } from "@/validation/errors";
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 import { MinLengthValidation } from "@/validation/validators/min-length/min-length-validation";
 
-const makeSut = (): MinLengthValidation =>
-  new MinLengthValidation(faker.database.column(), 3);
+// const makeSut = (): MinLengthValidation =>
+//   new MinLengthValidation(faker.database.column(), 3);
 
 describe("MinLengthValidation", () => {
   test("Should return error if value is invalid", () => {
@@ -11,9 +11,9 @@ describe("MinLengthValidation", () => {
     const error = sut.validate("123");
     expect(error).toEqual(new InvalidFieldError());
   });
-  test.skip("Should return falsy if value is valid", () => {
-    const sut = makeSut();
-    const error = sut.validate(faker.internet.email());
+  test("Should return falsy if value is valid", () => {
+    const sut = new MinLengthValidation("field", 5);
+    const error = sut.validate("12345");
     expect(error).toBeFalsy();
   });
 });
