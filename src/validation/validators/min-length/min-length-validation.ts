2207bff63fe8d63596be8d7a8fdb3d677c11b4c8
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-04 10:57:32 -0300

    feat: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from "@/validation/errors";
+import type { FieldValidation } from "@/validation/protocols/field-validation";
+
+export class MinLengthValidation implements FieldValidation {
+  constructor(
+    readonly field: string,
+    private readonly minLength: number,
+  ) {}
+  validate(value: string): Error | null {
+    return value.length >= this.minLength ? null : new InvalidFieldError();
+  }
+}
