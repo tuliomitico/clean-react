db6c091129875b93aa8a5cf3d397933d9b4f3677
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-05 17:38:44 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,9 @@
+import type { FieldValidation } from "@/validation/protocols/field-validation";
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error | null = null;
+  constructor(readonly field: string) {}
+  validate(value: string): Error | null {
+    return this.error;
+  }
+}
---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,16 @@
+import type { Validation } from "@/presentation/protocols/validation";
+import type { FieldValidation } from "@/validation/protocols/field-validation";
+
+export class ValidationComposite implements Validation {
+  constructor(private readonly validators: FieldValidation[]) {}
+  validate(fieldName: string, fieldValue: string): string | null {
+    const validators = this.validators.filter((v) => v.field === fieldName);
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue);
+      if (error) {
+        return error.message;
+      }
+    }
+    return null;
+  }
+}
