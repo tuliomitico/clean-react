37a24da9ea6acb7237185d9fee8fd63a8b1c1edf
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-07 14:00:52 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/builder/validation-builder.ts
@@ -0,0 +1,21 @@
+import type { FieldValidation } from "@/validation/protocols/field-validation";
+import { RequiredFieldValidation } from "@/validation/validators";
+
+export class ValidationBuilder {
+  private constructor(
+    private readonly fieldName: string,
+    private readonly validations: FieldValidation[],
+  ) {}
+  static field(fieldName: string): ValidationBuilder {
+    return new ValidationBuilder(fieldName, []);
+  }
+
+  required(): this {
+    this.validations.push(new RequiredFieldValidation(this.fieldName));
+    return this;
+  }
+
+  build(): FieldValidation[] {
+    return this.validations;
+  }
+}
---
+++ src/validation/validators/index.ts
@@ -0,0 +1,3 @@
+export * from "./required-field/required-field-validation";
+export * from "./min-length/min-length-validation";
+export * from "./email/email-validation";
