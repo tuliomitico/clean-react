6ca4861d2e33b61e66ef69bccb18aa7e82c3287a
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-09 13:19:41 -0300

    refactor: move factories to their own files

---
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,8 @@
+import type { Authentication } from "@/domain/usecases";
+import { makeAxiosHttpClient } from "@/main/factories/http/axios-http-client-factory";
+import { RemoteAuthentication } from "@/data/usecases/authentication/remote-authentication";
+import { makeApiUrl } from "@/main/factories/http/api-url-factory";
+
+export const makeRemoteAuthentication = (): Authentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient());
+};
