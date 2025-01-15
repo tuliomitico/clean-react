546cbf5266144292be7615809c29336d72c484d3
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-15 20:46:33 -0300

    feat: add env var to API_URL

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,11 +6,13 @@ import react from "@vitejs/plugin-react-swc";
  * https://vite.dev/config/
  * @type {import('vite').UserConfig}
  */
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
