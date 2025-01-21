5650d634cde5a9c9b6d64d7f2e58b584be879db0
tuliomitico <tulioneedforspeed@gmail.com>
2025-01-20 22:45:30 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostCLient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -59,15 +59,16 @@ describe("RemoteAddAccount", () => {
     const promise = sut.add(addAccountParams);
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
-  //   test("Should throw UnexpectedError if HttpPostClient returns 500", async () => {
-  //     const { sut, httpPostClientSpy } = makeSut();
-  //     httpPostClientSpy.response = {
-  //       statusCode: HttpStatusCode.serverError,
-  //     };
-  //     const authenticationParams = mockAuthentication();
-  //     const promise = sut.auth(authenticationParams);
-  //     await expect(promise).rejects.toThrow(new UnexpectedError());
-  //   });
+
+  test("Should throw UnexpectedError if HttpPostClient returns 500", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const addAccountParams = mockAddAccount();
+    const promise = sut.add(addAccountParams);
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
   //   test("Should throw UnexpectedError if HttpPostClient returns 404", async () => {
   //     const { sut, httpPostClientSpy } = makeSut();
   //     httpPostClientSpy.response = {
