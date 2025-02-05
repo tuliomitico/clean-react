3a5fa996d5f3103435ab93e9b7c73fc258df2bc3
tuliomitico <tulioneedforspeed@gmail.com>
2025-02-05 19:27:21 -0300

    feat: ensure SignUp change password confirmation status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -25,7 +25,7 @@ export function SignUp({
     nameError: "",
     emailError: "",
     passwordError: "",
-    passwordConfirmationError: "Campo obrigatório",
+    passwordConfirmationError: "",
     mainError: "",
   });
 
@@ -35,8 +35,13 @@ export function SignUp({
       nameError: validation?.validate("name", state.name) ?? "",
       emailError: validation?.validate("email", state.email) ?? "",
       passwordError: validation?.validate("password", state.password) ?? "",
+      passwordConfirmationError:
+        validation?.validate(
+          "passwordConfirmation",
+          state.passwordConfirmation,
+        ) ?? "",
     });
-  }, [state.name, state.email, state.password]);
+  }, [state.name, state.email, state.password, state.passwordConfirmation]);
 
   return (
     <div className={Styles.signup}>
