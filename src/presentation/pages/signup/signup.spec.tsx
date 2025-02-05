576b450fc6ff777533b8ae39ddde251ba0088f38
tuliomitico <tulioneedforspeed@gmail.com>
2025-02-05 19:23:41 -0300

    test: ensure SignUp change password status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -29,7 +29,7 @@ describe("SignUp Component", () => {
     Helper.testButtonIsDisabled(sut, "submit", true);
     Helper.testStatusForField(sut, "name", validationError);
     Helper.testStatusForField(sut, "email", validationError);
-    Helper.testStatusForField(sut, "password", "Campo obrigatório");
+    Helper.testStatusForField(sut, "password", validationError);
     Helper.testStatusForField(sut, "passwordConfirmation", "Campo obrigatório");
   });
 
@@ -45,5 +45,12 @@ describe("SignUp Component", () => {
     const { sut } = makeSut({ validationError });
     Helper.populateField(sut, "email");
     Helper.testStatusForField(sut, "email", validationError);
+  });
+
+  test("Should show password error if Validation fails", () => {
+    const validationError = faker.lorem.words();
+    const { sut } = makeSut({ validationError });
+    Helper.populateField(sut, "password");
+    Helper.testStatusForField(sut, "password", validationError);
   });
 });
