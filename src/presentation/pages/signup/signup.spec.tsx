2d6699faf1707a1ec2d03155be2a2a154f5ddbd9
tuliomitico <tulioneedforspeed@gmail.com>
2025-02-07 17:40:51 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -146,4 +146,11 @@ describe("SignUp Component", () => {
     await simulateValidSubmit(sut);
     expect(addAccountSpy.callsCount).toBe(1);
   });
+
+  test("Should not call AddAccount if form is invalid", async () => {
+    const validationError = faker.lorem.words();
+    const { sut, addAccountSpy } = makeSut({ validationError });
+    await simulateValidSubmit(sut);
+    expect(addAccountSpy.callsCount).toBe(0);
+  });
 });
