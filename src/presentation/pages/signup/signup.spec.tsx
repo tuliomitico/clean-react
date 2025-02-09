97d7dff680a4cdd5ef28ed72a01224dd2fd7de9c
tuliomitico <tulioneedforspeed@gmail.com>
2025-02-09 16:00:47 -0300

    test: ensure SignUp hide spinner and present error if AddAccount fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -8,6 +8,7 @@ import {
 import { SignUp } from "./signup";
 import { Helper, ValidationStub, AddAccountSpy } from "@/presentation/test";
 import { faker } from "@faker-js/faker";
+import { EmailInUseError } from "@/domain/errors";
 
 type SutTypes = {
   sut: RenderResult;
@@ -43,6 +44,15 @@ const simulateValidSubmit = async (
   const form = sut.getByTestId("form");
   fireEvent.submit(form);
   await waitFor(() => form);
+};
+
+const testElementText = (
+  sut: RenderResult,
+  fieldName: string,
+  text: string,
+): void => {
+  const el = sut.getByTestId(fieldName);
+  expect(el.textContent).toBe(text);
 };
 
 describe("SignUp Component", () => {
@@ -152,5 +162,14 @@ describe("SignUp Component", () => {
     const { sut, addAccountSpy } = makeSut({ validationError });
     await simulateValidSubmit(sut);
     expect(addAccountSpy.callsCount).toBe(0);
+  });
+
+  test("Should present error if AddAccount fails", async () => {
+    const { sut, addAccountSpy } = makeSut();
+    const error = new EmailInUseError();
+    jest.spyOn(addAccountSpy, "add").mockRejectedValueOnce(error);
+    await simulateValidSubmit(sut);
+    testElementText(sut, "main-error", error.message);
+    Helper.testChildCount(sut, "error-wrap", 1);
   });
 });
