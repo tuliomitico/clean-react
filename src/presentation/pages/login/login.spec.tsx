f858783f78e2d593f282b57575f0dab9948688dd
tuliomitico <tulioneedforspeed@gmail.com>
2025-02-09 16:05:09 -0300

    refactor: move duplicated code to a helper file

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -69,15 +69,6 @@ const simulateValidSubmit = async (
   await waitFor(() => form);
 };
 
-const testElementText = (
-  sut: RenderResult,
-  fieldName: string,
-  text: string,
-): void => {
-  const el = sut.getByTestId(fieldName);
-  expect(el.textContent).toBe(text);
-};
-
 describe("Login Component", () => {
   afterEach(cleanup);
 
@@ -156,7 +147,7 @@ describe("Login Component", () => {
     const error = new InvalidCredentialsError();
     jest.spyOn(authenticationSpy, "auth").mockRejectedValueOnce(error);
     await simulateValidSubmit(sut);
-    testElementText(sut, "main-error", error.message);
+    Helper.testElementText(sut, "main-error", error.message);
     Helper.testChildCount(sut, "error-wrap", 1);
   });
 
@@ -178,7 +169,7 @@ describe("Login Component", () => {
       .spyOn(saveAccessTokenMock, "save")
       .mockReturnValueOnce(Promise.reject(error));
     await simulateValidSubmit(sut);
-    testElementText(sut, "main-error", error.message);
+    Helper.testElementText(sut, "main-error", error.message);
     Helper.testChildCount(sut, "error-wrap", 1);
   });
 
--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -46,15 +46,6 @@ const simulateValidSubmit = async (
   await waitFor(() => form);
 };
 
-const testElementText = (
-  sut: RenderResult,
-  fieldName: string,
-  text: string,
-): void => {
-  const el = sut.getByTestId(fieldName);
-  expect(el.textContent).toBe(text);
-};
-
 describe("SignUp Component", () => {
   afterEach(cleanup);
 
@@ -169,7 +160,7 @@ describe("SignUp Component", () => {
     const error = new EmailInUseError();
     jest.spyOn(addAccountSpy, "add").mockRejectedValueOnce(error);
     await simulateValidSubmit(sut);
-    testElementText(sut, "main-error", error.message);
+    Helper.testElementText(sut, "main-error", error.message);
     Helper.testChildCount(sut, "error-wrap", 1);
   });
 });
