322a43696832c7e80d4ad654b56d7f0736e693b8
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-02-04 15:31:29 -0300

    chore: add cypress command

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: "http://localhost:5173",
-    supportFile: false,
+    supportFile: "src/main/test/cypress/support/index.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
